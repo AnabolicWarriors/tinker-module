eff20ba51c2d6e1207987b962161fc1612ea6544
Piorosen <aoikazto@gmail.com>
2022-12-04 00:46:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <serialib.h>
 
 using namespace std;
 
 int main(int argc, char** argv) { 
+    serialib serial;
+    
+
+    // Connection to serial port
+    char errorOpening = serial.openDevice("\\\\.\\COM4", 19200);
+
+    // If connection fails, return the error code otherwise, display a success message
+    if (errorOpening!=1) return errorOpening;
+    printf ("Successful connection to %s\n","\\\\.\\COM4");
+
+
+    // Display ASCII characters (from 32 to 128)
+    serial.writeString("i am chacha i love you!");
+
+    // Close the serial device
+    serial.closeDevice();
+
     cout << "done";
     return 0;
 }
